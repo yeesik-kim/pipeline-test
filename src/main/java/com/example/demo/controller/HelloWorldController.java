b3f994a943ac1831da8a05f56adf924c45ad3665
김의식/배포플랫폼개발팀/NHN <121150231+yeesik-kim@users.noreply.github.com>
2023-04-19 17:43:54 +0900

    Update HelloWorldController.java

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/")
 	public String helloWorld() {
-		return "Hello world 02";
+		return "Hello world 03";
 	}
 }
